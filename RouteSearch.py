ac115e0fab8adc6506b47457cdd28e340c439b6c
mkopanik <akopanpl@gmail.com>
2024-10-24 21:44:35 +0200

    dfs_bfs

---
+++ RouteSearch.py
@@ -0,0 +1,83 @@
+class RouteSearch:
+    def __init__(self,cities_map : CitiesMap):
+        self.nodes = cities_map.cities
+        self.size = cities_map.n
+        self.edges = cities_map.routes
+        # self.states = []
+
+    def dfs_all_paths(self, starting_node: int):
+        """
+        Perform DFS to explore all possible paths, both complete and incomplete.
+        
+        Parameters:
+        starting_node : int - the node from which to start the search
+        
+        Returns:
+        list : A list of paths. Path can be incomplete or complete.
+        """
+        # Stack for DFS
+        stack = [[starting_node]]
+        all_paths = []
+
+        while stack:
+            path = stack.pop()
+            last_node = path[-1]
+            
+            
+            # Record the current (incomplete or complete) path and its cost
+            all_paths.append(path)
+
+            # If the path includes all cities, check if it can return to the start
+            if len(path) == self.size:
+                return_to_start_cost = self.edges[last_node][starting_node]
+                if return_to_start_cost != 0:     # to make sure that we can return to start from there
+                    # Record the completed cycle (path returns to the start)
+                    all_paths.append(path + [starting_node])
+
+            # Explore all unvisited neighbors (continue DFS)
+            for neighbor in range(self.size - 1, -1, -1):  # reverse order to simulate stack behavior
+                if neighbor not in path and self.edges[last_node][neighbor] != 0:
+                    new_path = path + [neighbor]
+                    stack.append(new_path)
+                    
+        return all_paths    
+    
+    def bfs_all_paths(self, starting_node: int):
+        """
+        Perform BS to explore all possible paths, both complete and incomplete.
+        
+        Parameters:
+        starting_node : int - the node from which to start the search
+        
+        Returns:
+        list : A list of paths. Path can be incomplete or complete.
+        """
+        # queue for bfs
+        queue = [[starting_node]]
+        all_paths = []
+
+        while queue:
+            path = queue.pop(0)   # take first element from list (queue)
+            last_node = path[-1]
+            
+            # Record the current (incomplete or complete) path and its cost
+            all_paths.append(path)
+
+            # If the path includes all cities, check if it can return to the start
+            if len(path) == self.size:
+                return_to_start_cost = self.edges[last_node][starting_node]   # to make sure that we can return to start from there
+                if return_to_start_cost != 0:
+                    # Record the completed cycle (path returns to the start)
+                    all_paths.append(path + [starting_node])
+
+            # Explore all unvisited neighbors (continue bfs)
+            for neighbor in range(0, self.size):  # normal queue order
+                if neighbor not in path and self.edges[last_node][neighbor] != 0:
+                    new_path = path + [neighbor]
+                    queue.append(new_path)
+                   
+
+        return all_paths
+
+    def get_min_distance(self,state_tree):
+        """ Requires state tree object from for example dfs or bfs algo"""
